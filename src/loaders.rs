0628112c4d74dccd9cc3ca7aeeebd64988862734
Andre' Jenkins <mctech26@gmail.com>
2019-07-21 20:47:21 +0200

    Removed tests and updated comments

--- src/loaders.rs
+++ src/loaders.rs
@@ -6,11 +6,13 @@ use std::path::PathBuf;
 pub struct Loader;
 
 impl Loader {
+    // Copies the file "from" into "to"
     pub fn copy_file(from: String, to: String) -> std::io::Result<()>{
         copy(from, to)?;
         Ok(())
     }
 
+    // Constructs a mutable memory map of a file at "name"
     pub fn map_file_mut(name: String) -> std::io::Result<memmap::MmapMut> {
         let path : PathBuf = PathBuf::from(name);
         let file = OpenOptions::new()
@@ -23,11 +25,13 @@ impl Loader {
         Ok(mmap)
     }
 
+    // Copies the file "from" into "to", then mutably memory maps the "to" file.
     pub fn init_file_mut(from: String, to:String) -> std::io::Result<memmap::MmapMut> {
         Loader::copy_file(from, to.clone())?;
         Loader::map_file_mut(to)
     }
 
+    // Loads the contents of the file at "name" into "output"
     pub fn load_file_as_string(name: String, output: &mut String) -> std::io::Result<()> {
         let mut file = OpenOptions::new()
             .read(true)
