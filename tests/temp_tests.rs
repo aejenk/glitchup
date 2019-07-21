8ab68ed82a493517f5f673e3918d1ec7292d05cb
Calmynt <mctech26@gmail.com>
2019-07-21 15:34:29 +0200

    Implemented loader

--- tests/temp_tests.rs
+++ tests/temp_tests.rs
@@ -2,13 +2,14 @@ extern crate benders;
 
 #[test]
 // A temporary test to keep track of how to use memmap.
+// Also serves as an initialiser
 fn memmap_test() -> std::io::Result<()>{
     use memmap::MmapMut;
     use std::path::PathBuf;
     use std::fs::OpenOptions;
     use std::io::{Write, Read};
 
-    // Create/Load a file called "foo.txt"
+    // Create a file called "foo.txt"
     let path : PathBuf = PathBuf::from("foo.txt");
     let mut file = OpenOptions::new()
         .read(true)
@@ -45,31 +46,19 @@ fn memmap_test() -> std::io::Result<()>{
 
 #[test]
 // A temporary test to show functionality of basic mutation.
+// Also shows the use of Loaders
 fn basic_mut_test() -> std::io::Result<()>{
-    // libs needed for setup
-    use memmap::MmapMut;
-    use std::path::PathBuf;
-    use std::fs::OpenOptions;
-    use std::io::{Write,Read};
-
     // libs needed for benders
     use std::collections::HashMap;  
     use benders::mutations::*;      // import mutations
     use MutOptionVal::*;            // import enum from core
+    use benders::loaders::Loader;
 
-    // Create/Load a file called "foo.txt"
-    let path : PathBuf = PathBuf::from("foo.txt");
-    let mut file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(&path)?;
-
-    // Writes bytes to file
-    file.write_all(b"[!!!] Hello world!")?;
+    // file name for testing
+    let fname : String = String::from("tests/foo.txt");
 
     // Mutably memory map it
-    let mut mmap = unsafe { MmapMut::map_mut(&file)? };
+    let mut mmap = Loader::map_file_mut(fname.clone())?;
 
     // Initialise mutation and options
     let mut basic_mut = BasicMutation::default();
@@ -84,12 +73,8 @@ fn basic_mut_test() -> std::io::Result<()>{
     basic_mut.mutate(&mut *mmap, options);
 
     // Reload and read content of file
-    let mut file = OpenOptions::new()
-        .read(true)
-        .open(&path)?;
-
     let mut chars = String::new();
-    file.read_to_string(&mut chars)?;
+    Loader::load_file_as_string(fname, &mut chars)?;
 
     // Check characters were overwritten
     assert_eq!(chars.as_bytes(), b"[000] Hello world!");
