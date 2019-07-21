9ef608a64f4f740d6fc028cccd648a8a6e909c41
Calmynt <mctech26@gmail.com>
2019-07-22 00:06:04 +0200

    Added minor improvements

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,18 @@ mod options;
 mod loaders;
 mod benders;
 
-use options::TomlProcessor as Toml;
-use options::{MutConfig, MutOptionVal};
 use mutations::*;
-use loaders::Loader;
 use crate::benders::BasicBender;
 
-use serde::Deserialize;
-use std::collections::HashMap;
 
 fn main() -> std::io::Result<()> {
     // Load option file as a configuration
     let mut bender = BasicBender::new(String::from("options.toml"));
 
     // Initialise mutation
-    let mut basic_mut = BasicMutation::default();
+    let basic_mut = BasicMutation::default();
 
-    // Copy foo.txt into foo2.txt and memory map
+    // Initialise input and output file names for bender
     bender.init_file(
         String::from("foo.txt"),
         Some(String::from("foo2.txt"))
