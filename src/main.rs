3ea40d36f78a824b3bb4ee4ab8db731fc02e194d
Andre' Jenkins <mctech26@gmail.com>
2019-07-21 19:31:11 +0200

    Added option parsing with TOML

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
-use benders::mutations::*;
+mod mutations;
+mod opt_processor;
+mod loaders;
+
+use opt_processor::OptionProcessor as OPro;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct MainConfig {
+    mutation : MutationConfig
+}
+
+#[derive(Debug, Deserialize)]
+struct MutationConfig {
+    min : usize,
+    max : usize,
+    chunksize : usize
+}
 
 fn main() {
-    println!("Hello world!");
+    let options : MainConfig = OPro::parse(String::from("options.toml")).unwrap();
+
+    println!("ParsedOptions: {:#?}", options)
 }
