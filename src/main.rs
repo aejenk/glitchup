ced1b3369ff45373e2783d4bf3db9f2b9b1568ea
Calmynt <mctech26@gmail.com>
2019-07-27 14:22:18 +0200

    Impl MutConfig for MutationConfig

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,13 @@ mod benders;
 use mutations::basic_mutation::BasicMutation;
 use crate::benders::BasicBender;
 
-
 fn main() -> std::io::Result<()> {
-    // Initialise mutation
-    let basic_mut = &mut Box::new(BasicMutation::default());
+    // let basic_mut = &mut Box::new(BasicMutation::default());
 
     // Mutate the memory map according to the loaded configuration
-    BasicBender::new("options.toml", "foo.txt", None)
-        .mutate(basic_mut)
-        .mutate(basic_mut)
-        .mutate(basic_mut)
-        .mutate(basic_mut)
-        .mutate(basic_mut)
-        .mutate(basic_mut)
-        .mutate(basic_mut);
+    let bender = BasicBender::new("options.toml", "foo.txt", None);
+
+    println!("{:#?}", bender);
 
     Ok(())
 }
