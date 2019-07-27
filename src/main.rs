f9c2e545406846471df90ce3d711a089a3fb458b
Calmynt <mctech26@gmail.com>
2019-07-27 19:50:28 +0200

    Removed ununsed mut

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use mutations::basic_mutation::BasicMutation;
 use crate::benders::BasicBender;
 
 fn main() -> std::io::Result<()> {
-    let mut basic_mut = &mut Box::new(BasicMutation::default());
+    let basic_mut = &mut Box::new(BasicMutation::default());
 
     // Mutate the memory map according to the loaded configuration
     let mut bender = BasicBender::new("options.toml", "foo.txt", None);
