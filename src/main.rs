35295e551f5339ad2a1b89e1b3e5bac26bcdbb2b
Calmynt <mctech26@gmail.com>
2019-07-28 14:11:17 +0200

    Setup bender and mutation

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,23 @@
 //! a databender hasn't started yet.
 
 mod benders;
+use benders::KaBender;
+
+mod mutations;
+use mutations::void::Void;
 
 /// A do-nothin function that's sayin hello to you.
 fn main() {
-    println!("Hello world!");
+    let mutation = &mut Box::new(Void::default());
+
+    let mut bender = KaBender::new("Options.toml");
+
+    let loops = bender.config.loops.clone().unwrap_or(1);
+
+    for _ in 0..loops {
+        bender
+            .configure_mutation(mutation)
+            .mutate_with(mutation)
+            .restart();
+    }
 }
