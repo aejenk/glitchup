4a800089169bd2e443463bd0a11daec20f1a2b62
Calmynt <mctech26@gmail.com>
2019-09-14 19:25:05 +0200

    refactor: updated type aliases

--- src/mutation.rs
+++ src/mutation.rs
@@ -1,6 +1,6 @@
 use super::options::MutConfig;
 
-type Mut = Mutation + Send + Sync;
+type Mut = Box<Mutation + Send + Sync>;
 
 /// A `Mutation` trait to group every kind of `Mutation`.
 /// 
@@ -11,17 +11,17 @@ pub trait Mutation : std::fmt::Display + std::fmt::Debug + MutationClone{
 }
 
 pub trait MutationClone {
-    fn clone_box(&self) -> Box<Mut>;
+    fn clone_box(&self) -> Mut;
 }
 
 impl<T> MutationClone for T where
     T: 'static + Mutation + Clone + Send + Sync {
-        fn clone_box(&self) -> Box<Mut> {
+        fn clone_box(&self) -> Mut {
             Box::new(self.clone())
         }
 }
 
-impl Clone for Box<Mut> {
+impl Clone for Mut {
     fn clone(&self) -> Self {
         self.clone_box()
     }
