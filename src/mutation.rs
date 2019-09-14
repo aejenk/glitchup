64b254c3338c075162ce3c47acf5db33f98f3fde
Calmynt <mctech26@gmail.com>
2019-09-14 19:17:44 +0200

    feat: (unfortunately) made parallellism work

--- src/mutation.rs
+++ src/mutation.rs
@@ -3,24 +3,24 @@ use super::options::MutConfig;
 /// A `Mutation` trait to group every kind of `Mutation`.
 /// 
 /// Must also implement the `Display` trait.
-pub trait Mutation : std::fmt::Display + std::fmt::Debug + MutationClone {
+pub trait Mutation : std::fmt::Display + std::fmt::Debug + MutationClone{
     fn configure(&mut self, config: Box<&dyn MutConfig>);
     fn mutate(&mut self, data : &mut [u8]);
 }
 
 pub trait MutationClone {
-    fn clone_box(&self) -> Box<Mutation>;
+    fn clone_box(&self) -> Box<Mutation + Send + Sync>;
 }
 
 impl<T> MutationClone for T where
-    T: 'static + Mutation + Clone {
-        fn clone_box(&self) -> Box<Mutation> {
+    T: 'static + Mutation + Clone + Send + Sync {
+        fn clone_box(&self) -> Box<Mutation + Send + Sync> {
             Box::new(self.clone())
         }
 }
 
-impl Clone for Box<Mutation> {
-    fn clone(&self) -> Box<Mutation> {
+impl Clone for Box<Mutation + Send + Sync> {
+    fn clone(&self) -> Self {
         self.clone_box()
     }
 }
